e889d2c7c688f22832a73ebc53d047c67fc682bb
chedlykrati <chedly.krati@esprit.tn>
2022-06-28 20:44:46 +0100

    upload download files back-end

---
+++ src/main/java/uploadDownload/files/updownfiles/UpdownfilesApplication.java
@@ -0,0 +1,13 @@
+package uploadDownload.files.updownfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdownfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdownfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uploadDownload/files/updownfiles/UpdownfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package uploadDownload.files.updownfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdownfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
